8b70404067fd97e46bc8dad1a035089b73ccd19a
victor <jvictorcm@gmail.com>
2022-04-14 19:05:12 -0300

    spring initalizr

---
+++ src/main/java/br/com/victor/startUpPerformanceTest/StartUpPerformanceTestApplication.java
@@ -0,0 +1,13 @@
+package br.com.victor.startUpPerformanceTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUpPerformanceTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartUpPerformanceTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/victor/startUpPerformanceTest/StartUpPerformanceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.victor.startUpPerformanceTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartUpPerformanceTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
